0ecd9203a67714947478ecd1052f547e7cd43d09
Enes Demir <114176807+CivilDevoloperEnesDemir@users.noreply.github.com>
2023-03-08 14:39:10 +0300

    Add files via upload

---
+++ JAVA 101/Döngüler/03 NegatifDegGirKadTekSayTopla/ForExample.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+public class ForExample {
+    public static void main(String[] args){
+
+         /*
+
+            Java döngüler ile negatif bir değer girilene kadar kullanıcıdan
+            girişleri kabul eden ve girilen değerlerden tek sayıları
+            toplayıp ekrana basan programı yazıyoruz.
+
+         */
+
+        Scanner input=new Scanner(System.in);
+        int sayi, toplam = 0;
+        boolean kosul=true;
+
+
+
+        for(;kosul;){
+
+            System.out.print("Sayı girin:");
+            sayi = input.nextInt();
+
+            if (sayi >= 0) {
+
+                if (sayi % 2 != 0) {
+                    toplam = toplam + sayi;
+                }
+
+            }else {
+                kosul = false;
+            }
+
+        }
+
+
+
+        System.out.println("Girilen tek sayıların toplamı :"+toplam);
+
+
+    }
+}
