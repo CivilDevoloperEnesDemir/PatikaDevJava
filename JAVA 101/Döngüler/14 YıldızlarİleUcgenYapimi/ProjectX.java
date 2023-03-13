52508a8b4314a5402dc6a81bac0738cc68562744
Enes Demir <114176807+CivilDevoloperEnesDemir@users.noreply.github.com>
2023-03-13 08:25:13 +0300

    Add files via upload

---
+++ JAVA 101/Döngüler/14 YıldızlarİleUcgenYapimi/ProjectX.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class ProjectX {
+    public static void main(String[] args){
+        Scanner inp=new Scanner(System.in);
+
+        int n;
+        System.out.print("Bir sayı giriniz: ");
+        n=inp.nextInt();
+
+        for(int i=1;i<=n;i++){
+
+            for(int k=1;k<=(n-i);k++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=(2*i-1);j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+
+
+
+
+        }
+
+
+
+    }
+}
